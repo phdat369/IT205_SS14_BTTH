63c4fa5a3f71bb9a0910b425934bb3fa2936a7ab
phdat369 <phdat369@gmail.com>
2026-06-02 22:11:04 +0700

    Add files via upload

--- BTTHSS14.py
+++ BTTHSS14.py
@@ -12,7 +12,64 @@ def display_grades(book):
     print("------------------------------------------------------------------")
 
 def add_student(book):
+    while True:
+        id_input = input("Nhập mã học sinh mới: ").strip().upper()
+        valid = True
+        for student in book:
+            if student.get("id") == id_input:
+                print(f"Lỗi: Mã học sinh {id_input} đã tồn tại! Vui lòng nhập mã khác.")
+                valid = False
+                break
+        if valid == True:
+            break
+    new_name = input("Nhập tên học sinh: ").strip().title()
+    new_score_math = input("Nhập điểm Toán: ")
+    while not (new_score_math.count(".") == 1 and new_score_math.replace(".", "").isdigit()) or not (float(new_score_math) >= 0 and float(new_score_math) <= 10):
+        print("Điểm không hợp lệ, Vui lòng nhập lại: ")
+        new_score_math = input("Nhập điểm Toán: ")
+    new_score_english = input("Nhập điểm Anh: ")
+    while not (new_score_english.count(".") == 1 and new_score_english.replace(".", "").isdigit()) or not (float(new_score_english) >= 0 and float(new_score_english) <= 10):
+        print("Điểm không hợp lệ, Vui lòng nhập lại: ")
+        new_score_english = input("Nhập điểm Anh: ")
+    new_student = {
+        "id": id_input,
+        "name": new_name,
+        "info": (float(new_score_math),float(new_score_english))
+    }
+    book.append(new_student)
+    print(f"Thành công: Đã thêm sinh viên {id_input} vào hệ thống!")
 
+def update_scores(book):
+    id_input = input("Nhập mã sinh viên cần cập nhật: ").strip().upper()
+    valid = True 
+    for index,student in enumerate(book):
+        if id_input == student.get("id"):
+            new_score_math = input("Nhập điểm Toán mới: ")
+            while not (new_score_math.count(".") == 1 and new_score_math.replace(".", "").isdigit()) or not (float(new_score_math) >= 0 and float(new_score_math) <= 10):
+                print("Điểm không hợp lệ, Vui lòng nhập lại: ")
+                new_score_math = input("Nhập điểm Toán mới: ")
+            new_score_english = input("Nhập điểm Anh mới: ")
+            while not (new_score_english.count(".") == 1 and new_score_english.replace(".", "").isdigit()) or not (float(new_score_english) >= 0 and float(new_score_english) <= 10):
+                print("Điểm không hợp lệ, Vui lòng nhập lại: ")
+                new_score_english = input("Nhập điểm Anh mới: ")
+            student["info"] = (float(new_score_math),float(new_score_english))
+            print(f"Thành công: Đã cập nhật điểm cho sinh viên {id_input}!")
+            valid = False
+            break
+    if valid == True:
+        print("Không tìm thấy sinh viên này")
+
+def delete_student(book):
+    id_input = input("Nhập mã sinh viên cần cập nhật: ").strip().upper()
+    valid = True 
+    for index,student in enumerate(book):
+        if id_input == student.get("id"):
+            book.pop(index)
+            print(f"Thành công: Đã xóa hồ sơ sinh viên {id_input} khỏi hệ thống")
+            valid = False
+            break
+    if valid == True:
+        print("Không tìm thấy sinh viên này")
 while True:
     choose = input("""==== HỆ THỐNG QUẢN LÝ ĐIỂM SỐ ====
 1. Xem bảng điểm học sinh
@@ -26,14 +83,17 @@ Chọn chức năng (1-5): """)
         display_grades(grade_book)
         print()
     elif choose == "2":
-         add_student(grade_book)
+        add_student(grade_book)
         print()
     elif choose == "3":
+        update_scores(grade_book)
         print()
     elif choose == "4":
+        delete_student(grade_book)
         print()
     elif choose == "5":
         print("Chương trình kết thúc")
+        break
     else:
         print("Lựa chọn không hợp lệ")
         print()
